d2fdc24cb4de7af8b84c64cea1dc763bf9f16d7a
Jenan04 <jenanyasser1@gmail.com>
2025-10-05 16:33:12 +0300

    resolve conflict

---
+++ frontend/src/app/components/layoutWrapper.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "./Navbar";
+
+export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideNavbarRoutes = ["/signin", "/signup", "/forgot-password", "/verify-email"];
+  const shouldHideNavbar = hideNavbarRoutes.includes(pathname);
+
+  return (
+    <>
+      {!shouldHideNavbar && <Navbar />}
+      {children}
+    </>
+  );
+}
