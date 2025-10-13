30963325bc277e27014f7027af7900c379171e68
Jenan04 <jenanyasser1@gmail.com>
2025-10-13 17:23:09 +0300

    some edits

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -22,4 +22,28 @@ const getUserById = async (userId) => {
   return user;
 };
 
-module.exports = { getUserById };
+const getUserByEmail = async (email) => {
+  const user = await prisma.user.findUnique({
+    where: { email: email },
+    select: {
+      id: true,
+      email: true,
+      first_name: true,
+      last_name: true,
+      profil_picture: true,
+      role: true,
+      levels: {
+        select: {
+          name: true
+        }
+      }
+    }
+  });
+  return user; 
+};
+
+
+module.exports = { 
+  getUserById,
+  getUserByEmail,
+ };
