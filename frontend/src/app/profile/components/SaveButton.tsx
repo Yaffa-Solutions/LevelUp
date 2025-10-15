8118f4bced7f48da58acb18ebd0efdc7933e382b
Geneen <geneen850@gmail.com>
2025-10-15 21:49:03 +0300

    refactor: enhance SaveButton with loading state and  wrap it with flex container .

--- frontend/src/app/profile/components/SaveButton.tsx
+++ frontend/src/app/profile/components/SaveButton.tsx
@@ -3,19 +3,22 @@ type SaveButtonProps = {
   onClick: () => void;
   className?: string;
   disabled?: boolean;
+  loading?: boolean;
   text?: string;
   loadingText?: string;
 };
 
-const SaveButton = ({ onClick, className , disabled, text='Save Change', loadingText='Saving...'}: SaveButtonProps) => {
+const SaveButton = ({ onClick, className , disabled,loading=false, text='Save Change', loadingText='Saving...'}: SaveButtonProps) => {
   return (
+    <div className={`flex justify-end mt-4 ${className}`}>
     <button
       onClick={onClick}
-      disabled={disabled}
+      disabled={disabled || loading}
       className={`px-4 py-2 bg-gradient-to-r from-[#9333EA] to-[#2563EB] text-white rounded-full hover:bg-blue-700 transition ${className}`}
     >
       {disabled ? loadingText : text}
-    </button>
+      </button>
+      </div>
   );
 };
 
